fdd89c2cefe9261639f7c61b4a7939a1323d1f62
Johannes Rosenkranz Cordovez <jfronz@gmail.com>
2019-11-01 19:47:12 -0600

    Update mantenimiento.sh

--- mantenimiento.sh
+++ mantenimiento.sh
@@ -19,7 +19,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # *************************************************************************** #
 
-usuario_del_equipo="Johannes RC"
+usuario_del_equipo="user-name"
 
 # Ubucleaner
 Ubucleaner() {
